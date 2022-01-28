54fb509fedc782f99e82053d3c3b174a2e271612
Gaelle <gmorin.dev@gmail.com>
2022-01-28 13:40:39 +0100

    header: effect on header when scrolling added

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,16 @@ portfolioContents.forEach(portfolioContent => {
         portfolioContent.classList.add('portfolio__active')
     })
 })
+
+/*==================== ADD BOXSHADOW TO THE HEADER WHEN SCROLLING ====================*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll > 80 viewport's height, scroll-header class is added to the header tag.
+    if(this.pageYOffset >= 80) {
+        header.classList.add('scroll-header')
+    } else {
+        header.classList.remove('scroll-header')
+    }
+}
+
+window.addEventListener('scroll', scrollHeader)
