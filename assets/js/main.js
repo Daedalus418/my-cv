b7abaa9252b6e3e9d1fb14d32f42bb0fca1a5ec7
Gaelle <gmorin.dev@gmail.com>
2022-01-31 10:53:25 +0100

    menu: remove the menu on mobile screens when a link is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,14 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLinks = document.querySelectorAll('.nav__link')
+
+function linkClick() {
+    const navMenu = document.getElementById('js-nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+// When a nav__link is clicked, the show-menu class is removed from the nav__menu 
+navLinks.forEach(navLink => navLink.addEventListener('click', linkClick))
