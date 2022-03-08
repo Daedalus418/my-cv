630376e5dc22e09866f6c21236ef03e8799e7e5d
Gaelle <gmorin.dev@gmail.com>
2022-03-08 11:17:30 +0100

    contact-form: validation added

--- index.php
+++ index.php
@@ -519,21 +519,47 @@
         <?php
             $message_sent = false;
             $request_method = strtoupper($_SERVER['REQUEST_METHOD']);
+            const FIELD_REQUIRED = "Champs obligatoire";
+            const FIELD_INVALID = "Format de mail non valide";
 
-            if($request_method === 'POST') {
+            if ($request_method === 'POST') {
+                $fields = ['name', 'email', 'message'];
+                $errors = [];
+                $values = [];
                 $clean_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
                 $clean_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
                 $clean_message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-                $to = "morin.gaelle@gmail.com";
-                $body = "";
+                if (!filter_var($clean_email, FILTER_VALIDATE_EMAIL)) {
+                    $errors['email'] = FIELD_INVALID;
+                }
 
-                $body .= "From: ".$clean_name. "\r\n";
-                $body .= "Email: ".$clean_email. "\r\n";
-                $body .= "Message: ".$clean_message. "\r\n";
+                foreach ($fields as $field) {
+                    if (empty($_POST[$field])) {
+                        $errors[$field] = FIELD_REQUIRED;
+                    } else {
+                        $values[$field] = $_POST[$field];
+                    }
+                }
 
-                mail($to, "Bonjour", $body);
-                $message_sent = true;
+                if (filter_var($clean_email, FILTER_VALIDATE_EMAIL) && count($errors) === 0) {
+                    $values['email'] = trim($clean_email);
+                    $values['name'] = trim($clean_name);
+                    $values['message'] = trim($clean_message);
+
+                    $to = "morin.gaelle@gmail.com";
+                    $body = "";
+                    $body .= "From: " . $values['name'] . "\r\n";
+                    $body .= "Email: " . $values['email'] . "\r\n";
+                    $body .= "Message: " . $values['message'] . "\r\n";
+
+                    if (mail($to, "Bonjour !", $body)) {
+                        $message_sent = true;
+                        print_r($body);
+                    } else {
+                        echo "Mailer Error: " . error_get_last();
+                    }
+                }
             }
         ?>
 
@@ -563,9 +589,9 @@
                         </div>
                     </dl>
 
-                    <?php if($message_sent): ?>
+                    <?php if ($message_sent) : ?>
                         <p>Merci !</p>
-                    <?php elseif(!$message_sent): ?>
+                    <?php elseif (!$message_sent) : ?>
                         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="contact__form grid">
                             <div class="contact__inputs grid">
                                 <div class="contact__content">
